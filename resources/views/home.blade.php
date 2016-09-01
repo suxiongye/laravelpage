f7d01d75678952e6cead326c6d0520d0c3e73e93
su <suxiongye@21cn.com>
2016-09-01 18:58:04 +0800

    change the index font size

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,19 +16,19 @@
             $("#contact_content").hide();
 
             $("#about").click(function () {
-                $("#album_pic").hide();
+                $("#album_index").hide();
                 $('#contact_content').hide();
                 $("#about_content").show();
             });
 
             $("#contact").click(function () {
-                $("#album_pic").hide();
+                $("#album_index").hide();
                 $('#contact_content').show();
                 $("#about_content").hide();
             });
 
             $("#album").mouseover(function () {
-                $("#album_pic").show();
+                $("#album_index").show();
                 $('#contact_content').hide();
                 $("#about_content").hide();
             });
@@ -55,16 +55,23 @@
 
         #content_right {
             width: 80%;
+            min-height: 200px;
             float: left;
+            text-align: center;
+        }
+
+        #about_content {
+            min-height: 200px;
         }
 
         #img_index {
             max-width: 140px;
         }
 
-        .table tbody tr td{
+        .table tbody tr td {
             border: #FFFFFF;
         }
+
         .table tbody tr td {
             text-align: center;
             vertical-align: middle;
@@ -80,15 +87,16 @@
 <div id="content">
     <div id="content_left">
         <div style="margin: 30px 40px 30px ;list-style-type: none;font-family: 微软雅黑;">
-            <b> <a href="#" id="about">ABOUT</a><br>
-                <a href="#" id="contact">CONTACT</a></b>
+            <h4><b><a href="#" id="about">ABOUT</a></b></h4>
+
+            <h4><b><a href="#" id="contact">CONTACT</a></b></h4>
         </div>
         <ul style="float: left;">
             @foreach ($albums as $album)
                 <li style="margin: 20px 0;list-style-type: none;">
                     <div class="title" id="album">
                         <a href="{{ url('album/ajax/'.$album->id) }}" id="album_photo">
-                            <h4>{{ $album->name }}</h4>
+                            <h4><b>{{ $album->name }}</h4>
                         </a>
                     </div>
                     <div class="body">
@@ -99,20 +107,24 @@
         </ul>
     </div>
     <div id="content_right">
-        <div id="about_content"
-             style="float:left;text-align: center;">
-            <b>ABOUT</b><br>
-            <b>练金泳，1994年出生于中国广东。</b><br>
-            <b>Lian Jinyong, born in Guangdong, China, in 1994.</b><br>
-            <b>Locate in paris.</b>
+        <div id="about_content">
+            <h4><b>ABOUT</b></h4>
+
+            <h4><b>练金泳，1994年出生于中国广东。</b></h4>
+
+            <h4><b>Lian Jinyong, born in Guangdong, China, in 1994.</b></h4>
+
+            <h4><b>Locate in paris.</b></h4>
         </div>
 
-        <div id="contact_content"
-             style="float:left;text-align: center;">
-            <b>CONTACT</b><br>
-            <b>E-mail: <a href="simone199997@hotmail.com">simone199997@hotmail.com</a></b><br>
-            <b>facebook: <a href="https://www.facebook.com/">https://www.facebook.com/</a></b><br>
-            <b>Ins:<a href="https://www.instagram.com/ginlian/">https://www.instagram.com/ginlian/</a></b>
+        <div id="contact_content">
+            <h4><b>CONTACT</b><br></h4>
+
+            <h4><b>E-mail: <a href="simone199997@hotmail.com">simone199997@hotmail.com</a></b></h4>
+
+            <h4><b>facebook: <a href="https://www.facebook.com/">https://www.facebook.com/</a></b></h4>
+
+            <h4><b>Ins:<a href="https://www.instagram.com/ginlian/">https://www.instagram.com/ginlian/</a></b></h4>
         </div>
 
         <div id="album_index">
@@ -122,34 +134,34 @@
                     <td>
                         <img src="http://o9laew41e.bkt.clouddn.com/2016.jpg" id="img_index"/>
 
-                        <h3>PHOTOGRAPHY 2016</h3>
+                        <h4>PHOTOGRAPHY 2016</h4>
                     </td>
                     <td>
                         <img src="http://o9laew41e.bkt.clouddn.com/2015I.jpg" id="img_index"/>
 
-                        <h3>PHOTOGRAPHY 2015 I</h3>
+                        <h4>PHOTOGRAPHY 2015 I</h4>
                     </td>
                     <td>
                         <img src="http://o9laew41e.bkt.clouddn.com/2015II.jpg" id="img_index"/>
 
-                        <h3>PHOTOGRAPHY 2015 II</h3>
+                        <h4>PHOTOGRAPHY 2015 II</h4>
                     </td>
                 </tr>
                 <tr>
                     <td>
                         <img src="http://o9laew41e.bkt.clouddn.com/2014.jpg" id="img_index"/>
 
-                        <h3>PHOTOGRAPHY 2014</h3>
+                        <h4>PHOTOGRAPHY 2014</h4>
                     </td>
                     <td>
                         <img src="http://o9laew41e.bkt.clouddn.com/2013.jpg" id="img_index"/>
 
-                        <h3>PHOTOGRAPHY 2013</h3>
+                        <h4>PHOTOGRAPHY 2013</h4>
                     </td>
                     <td>
                         <img src="http://o9laew41e.bkt.clouddn.com/2012.jpg" id="img_index"/>
 
-                        <h3>PHOTOGRAPHY 2012</h3>
+                        <h4>PHOTOGRAPHY 2012</h4>
                     </td>
                 </tr>
                 </tbody>
